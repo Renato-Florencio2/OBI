f97b560e54b7b02d032446a71e4249e002579951
Renato Florencio <renatoflorencio180@gmail.com>
2026-08-28 20:57:45 -0300

    28/08/2026

--- Nivel2/2014/1fase/copa.cpp
+++ Nivel2/2014/1fase/copa.cpp
@@ -1,66 +1,79 @@
+// https://neps.academy/br/exercise/318
+
 #include <bits/stdc++.h>
 using namespace std;
+#define endl '\n'
+#define int long long
 
-typedef pair<int, int> ii;
-typedef pair<int, ii> edge;
-vector<edge> grafo, grafosub;
+struct Edge{
+	int weight;
+	int v1;
+	int v2;
+	int type;
+	
+	bool operator<(const Edge &other) const {
+		if(type != other.type) return type < other.type;
+		return weight < other.weight;
+	}
+};
 
-vector<int> uf;
+int vertices, arestas;
+vector<Edge> grafo;
+
+vector<int> uf, peso;
 int finduf(int x){
-    return (uf[x] == x) ? x : (uf[x] = finduf(uf[x]));
+	return (uf[x] == x) ? uf[x] : finduf(uf[x]);
+}
+bool sameuf(int n1, int n2){
+	n1 = finduf(n1); n2 = finduf(n2);
+	return n1 == n2;
 }
 void unionuf(int n1, int n2){
-    int x = finduf(n1), y = finduf(n2);
-    if(x == y) return;
-
-    uf[x] = y;
+	n1 = finduf(n1); n2 = finduf(n2);
+	
+	if(n1 == n2) return ;
+	if(peso[n1] < peso[n2]) swap(n1, n2);
+	
+	uf[n2] = n1;	
+	peso[n1] += peso[n2];
 }
 
 int kruskal(){
-    int total = 0;
+	int soma = 0;
+	for(auto &i:grafo){
+		if(!sameuf(i.v1, i.v2)){
+			soma += i.weight;
+			unionuf(i.v1, i.v2);
+		}
+	}
+	return soma;
+}	
 
-    for(auto i:grafo){
-        int x = finduf(i.second.first), y = finduf(i.second.second);
-        if(x != y){
-            unionuf(x, y);
-            total+=i.first;
-        }
-    }
-    for(auto i:grafosub){
-        int x = finduf(i.second.first), y = finduf(i.second.second);
-        if(x != y){
-            unionuf(x, y);
-            total+=i.first;
-        }
-    }
-    return total;
+__int32_t main(){
+
+	ios::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+
+	int v1, v2; cin>>vertices>>v1>>v2;
+	
+	for(int i = 0 ; i <= vertices ; i++){
+		uf.push_back(i);
+		peso.push_back(1);
+	}
+
+	for(int i = 0 ; i < v1 ; i++){
+		int n1, n2, peso; cin>>n1>>n2>>peso;
+		grafo.push_back({peso, n1, n2, 1});
+	}
+	
+	for(int i = 0 ; i < v2 ; i++){
+		int n1, n2, peso; cin>>n1>>n2>>peso;
+		grafo.push_back({peso, n1, n2, 2});
+	}
+	
+	sort(grafo.begin(), grafo.end());
+
+	cout<<kruskal()<<endl;
+
+	return 0;
 }
 
-int main() {
-
-    ios_base::sync_with_stdio(false);
-    cin.tie(0);
-
-    int cidades, ferr, rodo;
-    cin>>cidades>>ferr>>rodo;
-    
-    for(int i = 0 ; i <= cidades ; i++) uf.push_back(i);
-
-    while(ferr--){
-        int c1, c2, custo;
-        cin>>c1>>c2>>custo;
-        grafo.push_back({custo, {c1, c2}});
-    }
-    while(rodo--){
-        int c1, c2, custo;
-        cin>>c1>>c2>>custo;
-        grafosub.push_back({custo, {c1, c2}});
-    }
-    
-    sort(grafo.begin(), grafo.end());
-    sort(grafosub.begin(), grafosub.end());
-    int total = kruskal();
-    cout<<total<<"\n";
-
-    return 0;
-}
